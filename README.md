c2c79b4893f9ff7ba8791039d2d87220c43673d5
hrecht <hrecht@users.noreply.github.com>
2015-12-14 12:01:05 -0500

    naming conventions added to md

---
+++ README.md
@@ -0,0 +1,16 @@
+# Higher education site
+
+## Naming conventions
+* College types:
+ * pub = public
+ * pr = private
+ * np = non-profit
+ * fp = for-profit
+ * 4 = four-year
+ * 2 = two-year
+* Costs
+ * tu = tuition
+ * fees = fees
+ * rm = room
+ * bd = board
+* Example: tufeesrmbd_prnp4 = tuition + fees + room + board at four-year private non-profit college
--- scripts/processdata.R
+++ scripts/processdata.R
@@ -19,5 +19,9 @@ sax = "data/original/cbtrends-studentaid2015.xlsx"
 download.file("http://trends.collegeboard.org/sites/default/files/2015-trends-student-aid-source-data-final.xls", sa)
 
 #Read in relevant data
-#test
-fig1<-readWorkbook(cpx, sheet="Fig 1", colNames=T, rowNames=F, rows=2:6)
+#College Board prices
+#TABLE 2. Average Tuition and Fees and Room and Board (Enrollment-Weighted) in Current Dollars and in 2015 Dollars, 1971-72 to 2015-16
+#Only need first section - 2015 dollars
+tab2<-readWorkbook(cpx, sheet="Table 2", colNames=T, rowNames=F, rows=3:48)
+colnames(tab2) <- c("year_academic", "tufees_prnp4", "a", "tufees_pub4", "b", "tufees_pub2", "c", "tufeesrmbd_prnp4", "d", "tufeesrmbd_pub4", "e")
+tab2 <- tab2 %>% select (-c(a,b,c,d,e))
