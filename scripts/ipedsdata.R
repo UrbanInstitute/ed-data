c940da68327828ec816dcd237b54bbf616ec8cd2
hrecht <hrecht@users.noreply.github.com>
2015-12-15 13:27:14 -0500

    calculate enrollment stats - gitignore due to size for now:

--- scripts/ipedsdata.R
+++ scripts/ipedsdata.R
@@ -15,7 +15,7 @@ download.file("http://nces.ed.gov/ipeds/datacenter/data/HD2014_Dict.zip","data/o
 unzip("data/original/ipedsdictionary.zip", exdir="data/original/")
 
 #Choose key identifying variables
-colleges <- ids %>% select(unitid,instnm,city,stabbr,fips,sector,iclevel,control)
+colleges <- ids %>% select(unitid,fips,sector,iclevel,control)
 
 #Format variable name meanings correctly
 varnames <- read.csv("data/collegeidentifiers/varnames.csv", stringsAsFactors = F, sep='"')
@@ -43,14 +43,12 @@ write.csv(varnames,"data/collegeidentifiers/varnames.csv",row.names=F, na="")
 # CONTROL (HD2014)	3	Private for-profit
 # CONTROL (HD2014)	-3	{Not available}
 
+# 20 = undergraduate
+# 50 = graduate and professional
 
-########################################################################################################
-# Annual enrollment data from IPEDS
-########################################################################################################
-
-enrollment <- read.csv("data/enrollment/Data_12-14-2015.csv", stringsAsFactors = F)
-colnames(enrollment) <- tolower(colnames(enrollment))
-rawnames <- colnames(enrollment)
+#Total price for in-district students living on campus  2014-15 (DRVIC2014)	CINDON (DRVIC2014)
+#Total price for in-state students living on campus 2014-15 (DRVIC2014)	CINSON (DRVIC2014)
+#Total price for out-of-state students living on campus 2014-15 (DRVIC2014)	COTSON (DRVIC2014)
 
 #ef = enrollment fall
 #total = part time + full time
@@ -59,5 +57,59 @@ rawnames <- colnames(enrollment)
 #20 = undergraduate
 #50 = graduate and professional
 
-enrollment <- left_join(colleges,enrollment,by="unitid")
+########################################################################################################
+# Annual enrollment data from IPEDS
+########################################################################################################
 
+enrollment <- read.csv("data/enrollment/Data_12-14-2015.csv", stringsAsFactors = F)
+#useful column names
+colnames(enrollment) <- tolower(colnames(enrollment))
+rawnames <- colnames(enrollment)
+names <- as.data.frame(rawnames) %>% separate(rawnames, into = paste("V", 1:6, sep = "."))
+names$rawname <- rawnames
+names[ names == "" ] = NA
+names <- names %>% select(-V.6)
+table(names$V.1) 
+table(names$V.2)
+table(names$V.3)
+table(names$V.4)
+table(names$V.5)
+#years are all encoded in second position (V.2) - split into new column
+names <- names %>% mutate(year = gsub("[^[:digit:]]","",names$V.2)) %>% rename(category=V.1)
+names <- names %>% mutate(type = ifelse(category=="cindon"|category=="cinson"|category=="cotson", "totalprice",
+                                        ifelse(category=="efft"|category=="efpt"|category=="eftotal", "enrollment",
+                                               "identifier")))
+names <- names %>% mutate(studenttype = ifelse(category=="efft", "fulltime",
+                                               ifelse(category=="efpt", "parttime",
+                                                      ifelse(category=="eftotal", "total",
+                                                             ifelse(category=="cindon", "indistricton",
+                                                                    ifelse(category=="cinson", "instateon",
+                                                                           ifelse(category=="cotson", "outstateon",
+                                                                              NA)))))))
+names <- names %>% mutate(studentlevel = (as.numeric(gsub("[^[:digit:]]","",names$V.4)) + as.numeric(gsub("[^[:digit:]]","",names$V.5))))
+names <- names %>% mutate(temp1 = as.numeric(gsub("[^[:digit:]]","",names$V.4)), temp2 = as.numeric(gsub("[^[:digit:]]","",names$V.5)))
+names$temp1[is.na(names$temp1)] <- 0
+names$temp2[is.na(names$temp2)] <- 0
+names <- names %>% mutate(studentlevel = temp1+temp2) %>%
+  select(-temp1,-temp2)
+names$studentlevel[names$studentlevel == 0] <- NA
+
+enrollnames <- names %>% select(rawname,year, type,studenttype,studentlevel)
+write.csv(enrollnames,"data/enrollment/varnames.csv",row.names=F, na="")
+
+enrollment <- left_join(colleges,enrollment,by="unitid") %>% 
+  select(-institution.name)
+# Calculate means and sums by sector and state
+enrollsum <- enrollment %>% group_by(fips,sector) %>% summarise_each(funs(sum(., na.rm = TRUE)))
+enrollmean <- enrollment %>% group_by(fips,sector) %>% summarise_each(funs(mean(., na.rm = TRUE)))
+
+temp1 <- enrollsum %>% gather(rawname, sum, 6:243) %>% select(fips,sector,rawname,sum)
+temp2 <- enrollmean %>% gather(rawname, mean, 6:243) %>% select(fips,sector,rawname,mean)
+
+#Join together and add variable name columns
+enrollstats <- left_join(temp1,temp2,by=c("fips","sector","rawname"))
+enrollstats$rawname <- as.character(enrollstats$rawname)
+enrollstats <- left_join(enrollstats, enrollnames, by="rawname")
+enrollstats <- enrollstats %>% filter(sector >=1, sector <=5) %>% filter(type!="identifier")
+
+write.csv(enrollstats,"data/enrollment.csv",row.names=F, na="")
