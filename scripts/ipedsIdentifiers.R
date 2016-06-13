7e6e1672e082bd40dd5cac3e25d24c10626f126c
hrecht <hrecht@users.noreply.github.com>
2016-06-13 14:05:21 -0400

    define institutions

--- scripts/ipedsIdentifiers.R
+++ scripts/ipedsIdentifiers.R
@@ -5,6 +5,7 @@ library("jsonlite")
 library("dplyr")
 library("stringr")
 library("tidyr")
+library("openxlsx")
 
 ipedspath <- "/Users/hrecht/Documents/ipeds-scraper/"
 allfiles <- fromJSON(paste(ipedspath, "data/ipedsfiles.json", sep=""))
@@ -206,6 +207,14 @@ institutions <- institutions %>% filter(fips <= 56)
 institutions <- institutions %>% select(year, unitid, everything()) %>%
   arrange(year, unitid)
 
+########################################################################################################
+# Not all identifiers are available for early years - define as laid out by Martha Johnson
+# deggrant
+########################################################################################################
+
+# 2014 value labels
+labels2014 <- readWorkbook(paste(ipedspath, "dict/2014/hd2014.xlsx", sep=""), sheet="Frequencies")
+
 # Define deggrant for <2000
 table(institutions$deggrant)
 # DEGGRANT	1	Degree-granting
@@ -214,8 +223,178 @@ table(institutions$deggrant)
 # 3 and 4 used rarely in early 2000s for technical/vocational schools
 # 1: highest level offering is at least an associate's degree
 
-########## LEFT OFF HERE #############
-institutions <- institutions %>% mutate(temp = ifelse((year < 2000 & (level3==1 | level5==1 | level7==1 | level9==1 | level10==1)), 1, 0))
+# NAs are being treated as logical, not numeric - need to replace with something other than NA for variable defintions
+institutions$level3[is.na(institutions$level3)] <- -99
+institutions$level5[is.na(institutions$level5)] <- -99
+institutions$level7[is.na(institutions$level7)] <- -99
+institutions$level9[is.na(institutions$level9)] <- -99
+institutions$level10[is.na(institutions$level10)] <- -99
+
+institutions$deggrant2 <- institutions$deggrant
+institutions$deggrant2[institutions$year < 2000 & (institutions$level3==1 | institutions$level5==1 | institutions$level7==1 | 
+                                                institutions$level9==1 | institutions$level10==1)] <- 1
+institutions$deggrant2[institutions$year < 2000 & !(institutions$level3==1 | institutions$level5==1 | institutions$level7==1 | 
+                                                institutions$level9==1 | institutions$level10==1)] <- 2
+
+########################################################################################################
+# pset4flg - Postsecondary and Title IV institution indicator
+########################################################################################################
+
+# PSET4FLG	1	Title IV postsecondary institution
+# PSET4FLG	2	Non-Title IV postsecondary institution
+# PSET4FLG	3	Title IV NOT primarily postsecondary institution
+# PSET4FLG	4	Non-Title IV NOT primarily postsecondary institution
+# PSET4FLG	6	Non-Title IV postsecondary institution that is NOT open to the public
+# PSET4FLG	9	Institution is not active in current universe
+table(institutions$pset4flg)
+institutions$pset4flg2 <- institutions$pset4flg
+# institution is NOT 'not eligible for any of the above' federal financial aid programs
+institutions$pset4flg2[institutions$year < 1997 & institutions$finaid9!=1] <- 1
+institutions$pset4flg2[institutions$year < 1996 & is.na(institutions$finaid9)] <- 1
+# participates in Title IV federal financial aid programs
+institutions$pset4flg2[institutions$year >= 1996 & institutions$year < 2000 & institutions$opeflag==1] <- 1
+
+########################################################################################################
+# instcat - institutional category
+########################################################################################################
+
+# INSTCAT	1	Degree-granting, graduate with no undergraduate degrees
+# INSTCAT	2	Degree-granting, primarily baccalaureate or above
+# INSTCAT	3	Degree-granting, not primarily baccalaureate or above
+# INSTCAT	4	Degree-granting, associate's and certificates
+# INSTCAT	5	Nondegree-granting, above the baccalaureate
+# INSTCAT	6	Nondegree-granting, sub-baccalaureate
+# INSTCAT	-1	Not reported
+# INSTCAT	-2	Not applicable
+table(institutions$instcat)
+institutions$instcat2 <- institutions$instcat
+# Master's, Doctor's, or first-professional, no bachelor's, no associate's
+institutions$instcat2[institutions$year < 2004 & institutions$deggrant2==1 & 
+                        (institutions$level7==1 | institutions$level9==1 | institutions$level10==1) & 
+                        institutions$level5!=1 & institutions$level3!=1] <- 1
+# Bachelor's or higher, primarily bachelor's or higher
+institutions$instcat2[institutions$year < 2004 & institutions$deggrant2==1 & 
+                        (institutions$level5==1 | institutions$level7==1 | institutions$level9==1 | institutions$level10==1) & 
+                        institutions$degrees_pctbachplus > 0.5] <- 2
+# Bachelor's or higher, primarily below bachelor's
+institutions$instcat2[institutions$year < 2004 & institutions$deggrant2==1 & 
+                        (institutions$level5==1 | institutions$level7==1 | institutions$level9==1 | institutions$level10==1) & 
+                        institutions$degrees_pctbachplus <= 0.5] <- 3
+# Highest degree is associate's (may have post-bacc certificates)
+institutions$instcat2[institutions$year < 2004 & institutions$deggrant2==1 & institutions$level3==1 &
+                        institutions$level5!=1 & institutions$level7!=1 & institutions$level9!=1 & institutions$level10!=1] <- 4
+# post-bacc certificates, no degrees
+institutions$instcat2[institutions$year < 2004 & institutions$deggrant2==2 & institutions$hloffer >= 6] <- 5
+# no post-bacc certificates, no degrees
+institutions$instcat2[institutions$year < 2004 & institutions$deggrant2==2 & institutions$hloffer < 6] <- 6
+
+# Sector 0 = NA instcat (-2)
+institutions$instcat2[institutions$year < 2004 & institutions$sector==0] <- -2
+
+########################################################################################################
+# ccbasic for 1994 - 2004
+# can't make but don't need earlier years so not an issue
+# We'll generally be using carnegie for the latest year but sector over time
+########################################################################################################
+
+institutions$ccbasic2 <- institutions$ccbasic
+# doctoral and research level
+institutions$ccbasic2[institutions$year < 2000 & institutions$year >= 1994 & 
+                        (institutions$carnegie %in% c(11, 12, 13, 14))] <- 15
+institutions$ccbasic2[institutions$year < 2005 & institutions$year >= 2000 & 
+                        (institutions$carnegie %in% c(15,16))] <- 15
+
+# masters level
+institutions$ccbasic2[institutions$year < 2005 & institutions$year >= 1994 & 
+                        (institutions$carnegie %in% c(21, 22))] <- 20
+# special focus
+institutions$ccbasic2[institutions$year < 2005 & institutions$year >= 1994 & 
+                        (institutions$carnegie > 50)] <- 20
+
+########################################################################################################
+# Redefined carnegie for this project - carnegie_urban
+# 1 "public research" 2 "public masters" 3 "public associates" 4 "private nonprofit research" 5 "private nonprofit masters"
+# 6 "private nonprofit bachelors" 7 "for profit" 8 "small groups" 9 "special focus"
+########################################################################################################
+# Special institutions - graduate-students only or other special focus
+institutions$specialty <- 0
+institutions$specialty[(institutions$instcat2==1 | institutions$ccbasic2 > 23)] <- 1
+
+institutions$carnegie_urban <- 0
+### PUBLIC
+# public research
+institutions$carnegie_urban[institutions$ccbasic2 %in% c(15, 16, 17) & institutions$control==1] <- 1
+# public masters
+institutions$carnegie_urban[institutions$ccbasic2 %in% c(18, 19, 20) & institutions$control==1] <- 2
+# public associates
+institutions$carnegie_urban[institutions$instcat2 %in% c(3, 4) & institutions$control==1] <- 3
+
+### PRIVATE
+# private nonprofit research
+institutions$carnegie_urban[institutions$ccbasic2 %in% c(15, 16, 17) & institutions$control==2] <- 4
+# private nonprofit masters
+institutions$carnegie_urban[institutions$ccbasic2 %in% c(18, 19, 20) & institutions$control==2] <- 5
+# private nonprofit bachelors
+institutions$carnegie_urban[institutions$instcat2==2 & institutions$control==2 & institutions$carnegie_urban==0] <- 6
+
+# for profit
+institutions$carnegie_urban[institutions$control==3] <- 7
+
+#### SMALL GROUPS
+# public bachelors
+institutions$carnegie_urban[institutions$instcat2==2 & institutions$control==1 & institutions$carnegie_urban==0] <- 8
+# private nonprofit associates
+institutions$carnegie_urban[institutions$instcat2 %in% c(3,4) & institutions$control==2] <- 8
+
+# Special focus
+institutions$carnegie_urban[institutions$specialty==1] <- 9
+
+# non degree granting excluded
+institutions$carnegie_urban[institutions$deggrant2==2] <- NA
+
+
+########################################################################################################
+# Basic sector - sector_urban (named sectorv2 in Stata draft do files)
+# 1 "public two-year" 2 "public four-year" 3 "private nonprofit four-year" 4 "for profit" 5 "other" 6 "non-degree-granting"
+########################################################################################################
+# IPEDS version
+# SECTOR	0	Administrative Unit
+# SECTOR	1	Public, 4-year or above
+# SECTOR	2	Private not-for-profit, 4-year or above
+# SECTOR	3	Private for-profit, 4-year or above
+# SECTOR	4	Public, 2-year
+# SECTOR	5	Private not-for-profit, 2-year
+# SECTOR	6	Private for-profit, 2-year
+# SECTOR	7	Public, less-than 2-year
+# SECTOR	8	Private not-for-profit, less-than 2-year
+# SECTOR	9	Private for-profit, less-than 2-year
+
+institutions$sector_urban <- 0
+# public and fewer than 50% of degrees/certificates are bachelor's or higher
+institutions$sector_urban[institutions$instcat2 %in% c(3,4) & institutions$control==1] <- 1
+# public and more than 50% of degrees/certificates are bachelor's or higher
+institutions$sector_urban[institutions$instcat2==2 & institutions$control==1] <- 2
+# private and more than 50% of degrees/certificates are bachelor's or higher
+institutions$sector_urban[institutions$instcat2==2 & institutions$control==2] <- 3
+# for profit, any level
+institutions$sector_urban[institutions$control==3] <- 4
+# other degree-granting (small groups and special focus)
+institutions$sector_urban[institutions$carnegie_urban %in% c(8,9) & institutions$sector_urban==0] <- 5
+# non-degree-granting
+institutions$sector_urban[institutions$deggrant2==2] <- 6
+
+# Check
+table(institutions$carnegie_urban, institutions$sector_urban)
+
+write.csv(institutions, "data/ipeds/institutions.csv", row.names=F, na="")
+
+institutionskeep <- institutions %>% filter(sector_urban > 0 & pset4flg2==1)
+table(institutionskeep$year)
+
+########################################################################################################
+# Format institutions dataset
+# Fun: in 1986, unitids are all screwy
+########################################################################################################
 
 # Define universe
 # dt <- dt %>% filter(sector > 0 & pset4flg==1) %>%
