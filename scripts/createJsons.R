f4114895725b4208c187c6bb0bc30c811f44e323
hrecht <hrecht@users.noreply.github.com>
2016-07-11 15:09:54 -0400

    real function for generating graph jsons

--- scripts/createJsons.R
+++ scripts/createJsons.R
@@ -3,6 +3,59 @@
 library(dplyr)
 library(tidyr)
 library(jsonlite)
+library(openxlsx)
+
+# Path to Excel file with graph titles, notes, data sources
+textpath <- "/Users/hrecht/Box Sync/COMM/**Project Folders**/College Affordability (Lumina) Project/Getting data for Phase2/GraphText.xlsx"
+graphtext <- readWorkbook(textpath, sheet = 1)
+graphtext$section_number <- as.numeric(graphtext$section_number)
+
+# sectionn and graphn correspond to the location of the graph, specifically in the row in the Excel file that contains text attributes
+makeJson <- function(sectionn, graphn, dt, graphtype = "bar", series, categories, tickformat = "number", directlabels = FALSE, rotated = FALSE, xtype = "category", xlabel = NULL, ylabel = NULL) {
+  # Init json and attributes
+  graphjson <- NULL
+  metadata <- NULL
+  graphdata <- NULL
+  axis <- NULL
+  xaxis <- NULL
+  yaxis <- NULL
+  
+  graphdata$type <- graphtype
+  # Organize data series into "columns" for c3 specs
+  columns <- list()
+  for (s in seq_along(series)) {
+    columns[[s]] <- c(series[s], dt[,s+1])
+  }
+  graphdata$columns <- columns  
+  graphjson$data <- graphdata
+  
+  # Axis attributes
+  axis$rotated <- rotated
+  yaxis$tick$format <- tickformat
+  if (!is.null(xlabel)) {
+    xaxis$label <- xlabel
+  }
+  if (!is.null(ylabel)) {
+    yaxis$label <- ylabel
+  }
+  xaxis$type <- xtype
+  xaxis$categories <- categories
+  axis$x <- xaxis
+  axis$y <- yaxis
+  graphjson$axis <- axis
+  
+  # Text attributes - title, source, notes
+  row <- graphtext[graphtext$section_number==sectionn & graphtext$graph_number==graphn,]
+  graphjson$title <- row$title
+  metadata$sources <- row$sources
+  metadata$notes <- row$notes
+  graphjson$metadata <- metadata
+  
+  json <- toJSON(graphjson, auto_unbox=T)
+  
+  write(json, paste("graph-json/", sectionn, "_", graphn, ".json", sep=""))
+  return(json)
+}
 
 ########################################################################################################
 # Costs and subsidies (Section 2)
@@ -28,19 +81,6 @@ fig6$approp_local_change <- (fig6$approp_local_aged - local2000)/local2000
 # Save data as json
 fig6_min <- fig6 %>% select(year, approp_state_change, approp_local_change)
 
-# Once specs are done, make a function of this
-dtj <- toJSON(fig6_min, digits = 6)
-title <- "Change from FY 2000 in state and local support for higher education"
-type <- "line"
-ytype <- "%"
-src <- "State Higher Education Executive Officers Association."
-notes <- "Some notes about the chart if needed (maybe not)."
+json2_6 <- makeJson(sectionn = 2, graphn = 6, dt = fig6_min, graphtype = "line", series = c("State", "Local"), categories = fig6_min$year, tickformat = "percent", directlabels = FALSE, rotated = FALSE, xtype = "time", xlabel = NULL, ylabel = NULL) 
 
-graphj <- paste('{ "title": "', title, 
-                '", "data": ', dtj,
-                ', "type": "', type,
-                '", "axis": { "y": {"type": "', ytype,
-                '"} }, "source": "', src, 
-                '", "notes": "', notes, '"}', sep="")
 
-write(graphj, "graph-json/section2fig6.json")
