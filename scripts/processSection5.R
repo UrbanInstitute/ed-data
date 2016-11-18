b727efb00d444143d3f3cb0ed4e5c6c34b5ba512
Hou <vivhou@gmail.com>
2016-11-18 13:05:55 -0500

    revised graphs for section 5

--- scripts/processSection5.R
+++ scripts/processSection5.R
@@ -15,7 +15,7 @@ graphtext$toggle <- as.numeric(graphtext$toggle)
 #Figure 5-19
 fig5_19<- read.csv(paste(textpath, "Covering Expenses_time to degree/05_0190.csv", sep=""),stringsAsFactors=FALSE)
 json5_19<- makeJson(sectionn = 5, graphn = 19, dt = fig5_19, graphtype = "bar",
-                    series = c("Average Years Enrolled", "Average Years Elasped"),
+                    series = c("Average years enrolled", "Average years elasped"),
                     categories = fig5_19$category, tickformat = "number", rotated = FALSE, directlabels = TRUE)
 
 
@@ -36,7 +36,7 @@ json5_22<- makeJson(sectionn = 5, graphn = 22, dt = fig5_22, graphtype = "bar",
 
 fig5_23 <- read.csv(paste(textpath, "Covering expenses_time to degree/05_0230.csv", sep=""),stringsAsFactors=FALSE)
 json5_23 <- makeJson(sectionn = 5, graphn = 23, dt = fig5_23, graphtype = "bar",
-                     series = c("No debt", "Less than $10,000", "$10,000-$19,999", "$20,000-$29,999", "$30,000-$39,000", "$40,000 or More"),
+                     series = c("No debt", "Less than $10,000", "$10,000–$19,999", "$20,000–$29,999", "$30,000–$39,000", "$40,000 or More"),
                      categories = fig5_23$time_between_enroll_completion, tickformat = "percent", rotated = TRUE, directlabels = TRUE)
 
 #Figure 5-21
@@ -55,21 +55,22 @@ json5_20 <- makeJson(sectionn = 5, graphn = 20, dt = fig5_20, graphtype = "bar",
 
 #Figure 5-11
 fig5_11 <- read.csv(paste(textpath, "Covering expenses_working/05_0110.csv", sep=""),stringsAsFactors=FALSE)
-json5_11 <- makeJson(sectionn = 5, graphn = 11, dt = fig5_11, graphtype = "bar", series=c("Total", "Public Four-Year", "Private Nonprofit Four-Year", "Public Two-year", "For-profit"), set1=fig5_11[,c("all")], set2=fig5_11[,c("publicfour")],
+fig5_11$category <- gsub("-", "–", fig5_11$category) 
+json5_11 <- makeJson(sectionn = 5, graphn = 11, dt = fig5_11, graphtype = "bar", series=c("Total", "Public four-year", "Private nonprofit four-year", "Public two-year", "For-profit"), set1=fig5_11[,c("all")], set2=fig5_11[,c("publicfour")],
                      set3=fig5_11[,c("publictwo")], set4=fig5_11[,c("privatenonprofit")], set5=fig5_11[,c("forprofit")], categories = fig5_11$category, tickformat = "percent", rotated = TRUE, directlabels = TRUE)
 
 #Figure 5-10
 
 fig5_10 <- read.csv(paste(textpath, "Covering expenses_working/05_0100.csv", sep=""),stringsAsFactors=FALSE)
 json5_10 <- makeJson(sectionn = 5, graphn = 10, dt = fig5_10, graphtype = "bar",
-                     series = c("No earnings", "$1-$2,999", "$3,000-$5,999", "$6,000-$8,999", "$9,000-$11,999", "$12,000-$14,999", "$15,000 or more"),
+                     series = c("No earnings", "$1–$2,999", "$3,000–$5,999", "$6,000–$8,999", "$9,000–$11,999", "$12,000–$14,999", "$15,000 or more"),
                      categories = fig5_10$hours, tickformat = "percent", rotated = TRUE, directlabels = TRUE)
 
 #Figure 5-9
 
 fig5_9 <- read.csv(paste(textpath, "Covering expenses_working/05_0090.csv", sep=""),stringsAsFactors=FALSE)
 json5_9 <- makeJson(sectionn = 5, graphn = 9, dt = fig5_9, graphtype = "bar",
-                     series = c("No work", "1-5", "6-10", "11-15", "16-20", "21-30", "31-40", "41 or more"),
+                     series = c("No work", "1–5", "6–10", "11–15", "16–20", "21–30", "31–40", "41 or more"),
                      categories = fig5_9$hours, tickformat = "percent", rotated = TRUE, directlabels = TRUE)
 
 #Figure 5-8
