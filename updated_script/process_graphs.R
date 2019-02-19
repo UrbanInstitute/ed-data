7807f7f158a7290821365d5df154ee87798d4e1b
Daniel Wood <daniel.j.wood11@gmail.com>
2019-02-19 17:47:17 -0500

    consolidate and modularize script

--- updated_script/process_graphs.R
+++ updated_script/process_graphs.R
@@ -9,58 +9,59 @@ library(openxlsx)
 
 ## UPDATE ME!!!! ##
 # read in source code
-source('/Users/bchartof/Projects/ed-data/updated_script/makejsons_update.R')
+source('updated_script/makejsons_update.R')
 
 # controls verbosity of output
 DEBUG_FLAG <- TRUE
 
+# Metadata...this is the list of the sections and their subsections. They are tied together by index. 
+sections <- c(
+    "what-is-college/",
+    "cost-of-educating/",
+    "prices-and-expenses/",
+    "financial-aid/",
+    "covering-expenses/",
+    "after-college/",
+    "breaking-even/",
+    "student-profiles/"
+  )
+
+subSections <- list(
+    c("institutions/json/","students/json/"),
+    c("appropriations/json/","endowments/json/","subsidies/json/"),
+    c("forgone-earnings/json/","net-price/json/","room-and-board/json/","student-budgets/json/","tuition-and-fees/json/"),
+    c("federal/json","financial-need/json","grant-aid/json","institutional/json","other/json","state/json","tax-benefits/json"),
+    c("borrowing/json/","pre-college-income/json/","savings/json/","state-policies/json/","time-to-degree/json/","working-during-college/json/"),
+    c("breaking-even/json/","employment-after-college/json/","loan-repayment-and-default/json/","student-debt/json/","variation-in-earnings/json/"),
+    c("json/"),
+    c("json/")
+  )
 
 # path to all files and xlsx file name (xlsx in the SAME directory)
-GRAPH_TEXT_PATH <- "/Users/bchartof/Projects/ed-data/updated_script/"
+GRAPH_TEXT_PATH <- "updated_script/"
 GRAPHTEXT_FILENAME <- "Graphtext_test.xlsx"
+data_folder <- "csv-021919/"
+
+########### Important #################
+# Pick which section you want to generate
+selectedSection <- 2
+#######################################
+
+sections[selectedSection]
 
 # path to each of the data folders
-section_1_data_path <- "/Users/bchartof/Projects/ed-data/graph-data/test-csv/"
-section_1_output_base_path <- "/Users/bchartof/Projects/college-affordability.urban.org/pages/what-is-college/"
-section_1_subsection_paths <- c("institutions/json/","students/json/")
-
-section_2_data_path <- "/Users/bchartof/Projects/ed-data/graph-data/test-csv/"
-section_2_output_base_path <- "/Users/bchartof/Projects/college-affordability.urban.org/pages/cost-of-educating/"
-section_2_subsection_paths <- c("subsidies/json/","appropriations/json/","endowments/json/")
-
-section_3_data_path <- "/Users/bchartof/Projects/ed-data/graph-data/test-csv/"
-section_3_output_base_path <- "/Users/bchartof/Projects/college-affordability.urban.org/pages/prices-and-expenses/"
-section_3_subsection_paths <- c("tuition-and-fees/json/","room-and-board/json/","student-budgets/json/","forgone-earnings/json/","net-price/json/")
-
+section_data_path <- paste("graph-data/",data_folder,sep="")
+section_output_base_path <- paste("../college-affordability.urban.org_tester/pages/",sections[selectedSection],sep="")
+section_subsection_paths <- subSections[[selectedSection]]
 
 # read in graph df
 graph_text_df <- read.xlsx(paste(GRAPH_TEXT_PATH, GRAPHTEXT_FILENAME, sep=""))
 
 # section 1
-section_1_graph_text_df <- graph_text_df %>% 
+section_graph_text_df <- graph_text_df %>% 
   filter(section_number == 1)
 
-temp <- mapply(makeJson, section_1_graph_text_df$section_number, section_1_graph_text_df$subsection_number, 
-       section_1_graph_text_df$graph_number, text_file_path = GRAPH_TEXT_PATH, 
-       data_path = section_1_data_path, gt_file = GRAPHTEXT_FILENAME, debug = DEBUG_FLAG, 
-       output_path = paste(section_1_output_base_path, section_1_subsection_paths[section_1_graph_text_df$subsection_number],sep = ""))
-
-# section 2
-section_2_graph_text_df <- graph_text_df %>% 
-  filter(section_number == 2)
-
-temp <- mapply(makeJson, section_2_graph_text_df$section_number, section_2_graph_text_df$subsection_number, 
-       section_2_graph_text_df$graph_number, text_file_path = GRAPH_TEXT_PATH, 
-       data_path = section_2_data_path, gt_file = GRAPHTEXT_FILENAME, debug = DEBUG_FLAG, 
-       output_path = paste(section_2_output_base_path, section_2_subsection_paths[section_2_graph_text_df$subsection_number],sep = ""))
-
-
-# section 3
-section_3_graph_text_df <- graph_text_df %>% 
-  filter(section_number == 3)
-
-temp <- mapply(makeJson, section_3_graph_text_df$section_number, section_3_graph_text_df$subsection_number, 
-       section_3_graph_text_df$graph_number, text_file_path = GRAPH_TEXT_PATH, 
-       data_path = section_3_data_path, gt_file = GRAPHTEXT_FILENAME, debug = DEBUG_FLAG, 
-       output_path = paste(section_3_output_base_path, section_3_subsection_paths[section_3_graph_text_df$subsection_number],sep = ""))
-
+temp <- mapply(makeJson, section_graph_text_df$section_number, section_graph_text_df$subsection_number, 
+       section_graph_text_df$graph_number, text_file_path = GRAPH_TEXT_PATH, 
+       data_path = section_data_path, gt_file = GRAPHTEXT_FILENAME, debug = DEBUG_FLAG, 
+       output_path = paste(section_output_base_path, section_subsection_paths[section_graph_text_df$subsection_number],sep = ""))
